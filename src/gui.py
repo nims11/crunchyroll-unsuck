0623d9d858b0ee1c357140ecc0ee39fc44eec5c3
Nimesh Ghelani <nimeshghelani@gmail.com>
2018-04-26 17:42:16 -0400

    Make widget a type of baselayout

--- src/gui.py
+++ src/gui.py
@@ -19,17 +19,6 @@ class BaseObject(object):
         pass
 
 
-class Widget(BaseObject):
-    def __init__(self, parent):
-        self.parent = parent
-        if parent != None:
-            self.parent.add_child(self)
-
-
-    def redraw(self):
-        pass
-
-
 class BaseLayout(BaseObject):
     """ Base Layout unit. Can only contain one child
     """
@@ -52,7 +41,17 @@ class BaseLayout(BaseObject):
         self.children.append(child)
 
 
-    def compute_dimensions(self):
+    def compute_dimensions(self, parent_height=None, parent_width=None, parent_x = None, parent_y = None):
+        if parent_x == None:
+            self._x = 0
+        else:
+            self._x = parent_x
+
+        if parent_y == None:
+            self._y = 0
+        else:
+            self._y = parent_y
+
         if self.height.type == Value.VAL_ABSOLUTE:
             self._height = int(self.height.value)
 
@@ -61,22 +60,18 @@ class BaseLayout(BaseObject):
 
         if self.parent is not None:
             if self.height.type == Value.VAL_RELATIVE:
-                self._height = int(self.parent._height*self.height.value)
+                self._height = int(parent_height*self.height.value)
             if self.width.type == Value.VAL_RELATIVE:
-                self._width = int(self.parent._width*self.width.value)
+                self._width = int(parent_width*self.width.value)
         elif self.width.type == Value.VAL_RELATIVE or self.height.type == Value.VAL_RELATIVE:
             raise Exception('root layout cannot have relative dimensions')
-        else:
-            self._x = 0
-            self._y = 0
 
 
     def redraw(self):
-        self.compute_dimensions()
+        if self.parent == None:
+            self.compute_dimensions()
         for child in self.children:
-            if isinstance(child, BaseLayout):
-                child._x = self._x
-                child._y = self._y
+            child.compute_dimensions(self._height, self._width, self._x, self._y)
             child.redraw()
 
 
@@ -94,11 +89,11 @@ class HorizontalLayout(StackedLayout):
     """ child layouts arranged horizontally
     """
     def redraw(self):
-        self.compute_dimensions()
+        if self.parent == None:
+            self.compute_dimensions()
         cumulative_width = 0
         for child in self.children:
-            child._x = self._x + cumulative_width
-            child._y = self._y
+            child.compute_dimensions(self._height, self._width, self._x + cumulative_width, self._y)
             child.redraw()
             cumulative_width += child._width
 
@@ -107,13 +102,35 @@ class VerticalLayout(StackedLayout):
     """ child layouts arranged vertically
     """
     def redraw(self):
-        self.compute_dimensions()
+        if self.parent == None:
+            self.compute_dimensions()
         cumulative_height = 0
         for child in self.children:
-            child._x = self._x
-            child._y = self._y + cumulative_height
+            child.compute_dimensions(self._height, self._width, self._x, self._y + cumulative_height)
             child.redraw()
             cumulative_height += child._height
+
+
+class Widget(BaseLayout):
+    def __init__(self, parent):
+        super().__init__(None, None, parent)
+        if parent == None:
+            raise Exception("Widget needs parents")
+
+
+    def add_child(self, child):
+        raise Exception("Widget cannot have children")
+
+
+    def compute_dimensions(self, parent_height=None, parent_width=None, parent_x = None, parent_y = None):
+        self._height = parent_height
+        self._width = parent_width
+        self._x = parent_x
+        self._y = parent_y
+
+
+    def redraw(self):
+        pass
 
 
 class DummyWidget(Widget):
@@ -124,27 +141,52 @@ class DummyWidget(Widget):
     def redraw(self):
         if self.window is not None:
             pass
-        self.window = curses.newwin(self.parent._height, self.parent._width, self.parent._y, self.parent._x)
+        self.window = curses.newwin(self._height, self._width, self._y, self._x)
         self.window.border()
-        self.window.addstr(0, 0, "%dx%d - (%d,%d)" % (self.parent._height, self.parent._width, self.parent._y, self.parent._x))
+        self.window.addstr(0, 0, "%dx%d - (%d,%d)" % (self._height, self._width, self._y, self._x))
         self.window.refresh()
+
+
+class ContainerWidget(Widget):
+    def __init__(self, parent, border=False, title=None):
+        super().__init__(parent)
+        self.window = None
+        self.title = title
+        self.border = border
+
+
+    def add_child(self, child):
+        if self.children:
+            raise Exception("ContainerWidget cannot have more than one children")
+        self.children.append(child)
+
+
+    def redraw(self):
+        if self.window is not None:
+            del self.window
+        self.window = curses.newwin(self._height, self._width, self._y, self._x)
+        if self.border:
+            self.window.border()
+        self.window.addstr(0, 0, self.title)
+        self.window.refresh()
+
+        for child in self.children:
+            if self.border:
+                child.compute_dimensions(self._height-2, self._width-2, self._x+1, self._y + 1)
+            else:
+                child.compute_dimensions(self._height, self._width, self._x, self._y)
+            child.redraw()
 
 
 def main(stdscr):
     root = BaseLayout(Value(curses.COLS), Value(curses.LINES), None)
-    l1 = VerticalLayout(Value(1, Value.VAL_RELATIVE), Value(1, Value.VAL_RELATIVE), root)
-    l2 = BaseLayout(Value(1, Value.VAL_RELATIVE), Value(0.33, Value.VAL_RELATIVE), l1)
-    l3 = HorizontalLayout(Value(1, Value.VAL_RELATIVE), Value(0.33, Value.VAL_RELATIVE), l1)
-    l4 = BaseLayout(Value(1, Value.VAL_RELATIVE), Value(0.34, Value.VAL_RELATIVE), l1)
+    l1 = HorizontalLayout(Value(1, Value.VAL_RELATIVE), Value(1, Value.VAL_RELATIVE), root)
+    l2 = BaseLayout(Value(0.3, Value.VAL_RELATIVE), Value(1, Value.VAL_RELATIVE), l1)
+    l3 = BaseLayout(Value(0.7, Value.VAL_RELATIVE), Value(1, Value.VAL_RELATIVE), l1)
 
-    DummyWidget(l2)
-    DummyWidget(l4)
-    l5 = BaseLayout(Value(0.33, Value.VAL_RELATIVE), Value(1, Value.VAL_RELATIVE), l3)
-    l6 = BaseLayout(Value(0.33, Value.VAL_RELATIVE), Value(1, Value.VAL_RELATIVE), l3)
-    l7 = BaseLayout(Value(0.34, Value.VAL_RELATIVE), Value(1, Value.VAL_RELATIVE), l3)
-    DummyWidget(l5)
-    DummyWidget(l6)
-    DummyWidget(l7)
+    c1 = ContainerWidget(l2, True, "Anime")
+    ContainerWidget(l3, True, "Episodes")
+    DummyWidget(c1)
 
     root.redraw()
 
